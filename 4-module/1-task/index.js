f3c95603042d3cafcc9aec5a9fab27765948aa09
Viktoriia Kulko <vichka.sapronowa@gmail.com>
2020-12-28 20:49:57 -0500

    add 4 module 1 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,6 +3,14 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+
 function makeFriendsList(friends) {
   // ваш код...
+    const ul = document.createElement('ul');
+    let li = '';
+
+    friends.forEach(friend => li += `<li>${friend.firstName} ${friend.lastName}</li>`);
+    ul.innerHTML = li;
+
+    return ul;
 }
