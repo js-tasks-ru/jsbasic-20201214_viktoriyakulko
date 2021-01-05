646c87b404bfd541560ac8d065a7e9d2f557c91a
Viktoriia Kulko <vichka.sapronowa@gmail.com>
2021-01-04 20:13:13 -0500

    add 5 module 2 task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
 function toggleText() {
   // ваш код...
+  const toggleTextButton = document.querySelector('.toggle-text-button');
+ 
+  toggleTextButton.addEventListener('click', () => text.hidden = !text.hidden);
 }
