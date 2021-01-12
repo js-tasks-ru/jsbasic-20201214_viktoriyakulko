f822a2b7a1fc55a6ee4698b6000ae89b708188fa
Viktoriia Kulko <vichka.sapronowa@gmail.com>
2021-01-12 18:58:20 -0500

    add 6 module 1 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -24,11 +24,58 @@
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      },
  *
  * @constructor
  */
 export default class UserTable {
   constructor(rows) {
+    this._titles = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+    this._rows = rows;
+    this.elem = this._render();
   }
+
+  _render() {
+    const table = document.createElement('table');
+    const template = tableTemplate({
+      titles: this._titles,
+      rows: this._rows
+    });
+
+    table.innerHTML = template;
+    table.addEventListener('click', this._onTableClick);
+
+    return table;
+  }
+
+  _onTableClick(e) {
+    const target = e.target;
+
+    if (target.dataset.component != 'button-remove') return;
+    target.closest('tr').remove();
+  }
+}
+
+function tableTemplate({ titles = [], rows = '' } = {}) {
+  return `
+    <thead>
+      <tr>
+        ${titles.map(title => `<th>${title}</th>`).join('')}
+      </tr>
+    </thead>
+    <tbody>
+      ${rows.map(row => tableRowTemplate({ cells: row })).join('')}
+    </tbody>
+  `;
+}
+
+function tableRowTemplate({ cells = {}} = {}) {
+  return `
+    <tr>
+      ${Object.values(cells).map(cell => `<td>${cell}</td>`).join('')}
+      <td>
+        <button data-component="button-remove">X</button>
+      </td>
+    </tr>
+  `;
 }
