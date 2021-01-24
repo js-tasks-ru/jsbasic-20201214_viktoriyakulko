0afaed4779a9be8d2ee6582bc43e64c43ced1e83
Viktoriia Kulko <vichka.sapronowa@gmail.com>
2021-01-23 21:27:28 -0500

    add 8 module 4 task, fix 7 module 2 task

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -8,6 +8,7 @@ export default class Cart {
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
+    this.modal = new Modal();
 
     this.addEventListeners();
   }
@@ -54,57 +55,57 @@ export default class Cart {
 
   renderProduct(product, count) {
     return createElement(`
-    <div class="cart-product" data-product-id="${
-      product.id
-    }">
-      <div class="cart-product__img">
-        <img src="/assets/images/products/${product.image}" alt="product">
-      </div>
-      <div class="cart-product__info">
-        <div class="cart-product__title">${escapeHtml(product.name)}</div>
-        <div class="cart-product__price-wrap">
-          <div class="cart-counter">
-            <button type="button" class="cart-counter__button cart-counter__button_minus">
-              <img src="/assets/images/icons/square-minus-icon.svg" alt="minus">
-            </button>
-            <span class="cart-counter__count">${count}</span>
-            <button type="button" class="cart-counter__button cart-counter__button_plus">
-              <img src="/assets/images/icons/square-plus-icon.svg" alt="plus">
-            </button>
+      <div class="cart-product" data-product-id="${
+        product.id
+      }">
+        <div class="cart-product__img">
+          <img src="/assets/images/products/${product.image}" alt="product">
+        </div>
+        <div class="cart-product__info">
+          <div class="cart-product__title">${escapeHtml(product.name)}</div>
+          <div class="cart-product__price-wrap">
+            <div class="cart-counter">
+              <button type="button" class="cart-counter__button cart-counter__button_minus">
+                <img src="/assets/images/icons/square-minus-icon.svg" alt="minus">
+              </button>
+              <span class="cart-counter__count">${count}</span>
+              <button type="button" class="cart-counter__button cart-counter__button_plus">
+                <img src="/assets/images/icons/square-plus-icon.svg" alt="plus">
+              </button>
+            </div>
+            <div class="cart-product__price">€${(product.price * count).toFixed(2)}</div>
           </div>
-          <div class="cart-product__price">€${product.price.toFixed(2)}</div>
         </div>
       </div>
-    </div>`);
+    `);
   }
 
   renderOrderForm() {
-    return createElement(`<form class="cart-form">
-      <h5 class="cart-form__title">Delivery</h5>
-      <div class="cart-form__group cart-form__group_row">
-        <input name="name" type="text" class="cart-form__input" placeholder="Name" required value="Santa Claus">
-        <input name="email" type="email" class="cart-form__input" placeholder="Email" required value="john@gmail.com">
-        <input name="tel" type="tel" class="cart-form__input" placeholder="Phone" required value="+1234567">
-      </div>
-      <div class="cart-form__group">
-        <input name="address" type="text" class="cart-form__input" placeholder="Address" required value="North, Lapland, Snow Home">
-      </div>
-      <div class="cart-buttons">
-        <div class="cart-buttons__buttons btn-group">
-          <div class="cart-buttons__info">
-            <span class="cart-buttons__info-text">total</span>
-            <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(
-              2
-            )}</span>
-          </div>
-          <button type="submit" class="cart-buttons__button btn-group__button button">order</button>
+    return createElement(`
+      <form class="cart-form">
+        <h5 class="cart-form__title">Delivery</h5>
+        <div class="cart-form__group cart-form__group_row">
+          <input name="name" type="text" class="cart-form__input" placeholder="Name" required value="Santa Claus">
+          <input name="email" type="email" class="cart-form__input" placeholder="Email" required value="john@gmail.com">
+          <input name="tel" type="tel" class="cart-form__input" placeholder="Phone" required value="+1234567">
         </div>
-      </div>
-    </form>`);
+        <div class="cart-form__group">
+          <input name="address" type="text" class="cart-form__input" placeholder="Address" required value="North, Lapland, Snow Home">
+        </div>
+        <div class="cart-buttons">
+          <div class="cart-buttons__buttons btn-group">
+            <div class="cart-buttons__info">
+              <span class="cart-buttons__info-text">total</span>
+              <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(2)}</span>
+            </div>
+            <button type="submit" class="cart-buttons__button btn-group__button button">order</button>
+          </div>
+        </div>
+      </form>
+    `);
   }
 
   renderModal() {
-    const modal = new Modal();
     const body = createElement('<div></div>');
     const products = this.cartItems.map(item => this.renderProduct(item.product, item.count));
     const orderForm = this.renderOrderForm();
@@ -112,15 +113,15 @@ export default class Cart {
     products.forEach(product => body.append(product));
     body.append(orderForm);
 
-    modal.setTitle('Your order');
-    modal.setBody(body);
-    modal.open();
+    this.modal.setTitle('Your order');
+    this.modal.setBody(body);
+    this.modal.open();
 
-    body.addEventListener('click', this.onProductsClick);
+    body.addEventListener('click', this.onProductClick);
     orderForm.addEventListener('submit', this.onSubmit);
   }
 
-  onProductsClick = (e) => {
+  onProductClick = (e) => {
     const button = e.target.closest('.cart-counter__button');
 
     if (!button) return false;
@@ -137,14 +138,58 @@ export default class Cart {
   }
 
   onProductUpdate(cartItem) {
-    // ...ваш код
+    if (document.body.classList.contains('is-modal-open')) {
+      const productId = cartItem.product.id;
+      const modalBody = document.querySelector('.modal__body');
+      const product = modalBody.querySelector(`[data-product-id="${productId}"]`);
+      const productCount = modalBody.querySelector(`[data-product-id="${productId}"] .cart-counter__count`);
+      const productPrice = modalBody.querySelector(`[data-product-id="${productId}"] .cart-product__price`);
+      const infoPrice = modalBody.querySelector(`.cart-buttons__info-price`);
+
+      if (cartItem.count == 0) {
+        product.remove();
+      }
+
+      if (this.getTotalCount() == 0) {
+        this.modal.close();
+      }
+
+      productCount.innerHTML = cartItem.count;
+      productPrice.innerHTML = `€${(cartItem.product.price * cartItem.count).toFixed(2)}`;
+      infoPrice.innerHTML = `€${this.getTotalPrice().toFixed(2)}`;
+    }
 
     this.cartIcon.update(this);
   }
 
-  onSubmit = (e) => {
+  onSubmit = async (e) => {
     e.preventDefault();
-    console.log('submit');
+
+    const form = document.querySelector('.cart-form');
+    const buttonSubmit = form.querySelector('[type="submit"]');
+
+    buttonSubmit.classList.add('is-loading');
+
+    let response = await fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: new FormData(form)
+    });
+
+    if (response.ok) {
+      this.cartItems.length = 0;
+      this.modal.setTitle('Success!');
+      this.modal.setBody(createElement(`
+        <div class="modal__body-inner">
+          <p>
+            Order successful! Your order is being cooked :)
+            <br>
+            We’ll notify you about delivery time shortly.
+            <br>
+            <img src="/assets/images/delivery.gif">
+          </p>
+        </div>
+      `));
+    }
   };
 
   addEventListeners() {
